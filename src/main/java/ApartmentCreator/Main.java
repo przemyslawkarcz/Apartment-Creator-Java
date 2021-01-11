019fc726e6f03884968cbed47287f842a8fc0f1f
Przemyslaw Karcz <przemyslawkarcz@gmail.com>
2021-01-11 19:44:26 +0100

    description of Main's method changed

--- src/main/java/ApartmentCreator/Main.java
+++ src/main/java/ApartmentCreator/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Creates apartment which consists of 1 bathroom, 1 kitchen and 1 living room with default features,
-        // and with certain number of bedrooms given by user.
+        // Main method which creates apartment which by default consists of 1 bathroom, 1 kitchen and 1 living room
+        // with other default features and with certain number of bedrooms given by user.
         createsApartment();
 
     }
